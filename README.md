d8e93c31f75291aa813593fbfb7f3d7a57e5c9ca
Rajendran R <rajendranr-5483@users.noreply.github.com>
2020-03-11 18:26:55 +0530

    Files added to run the application

---
+++ README.md
@@ -0,0 +1,11 @@
+# Vue-Js and .Net Core
+
+Getting started with Vue Js and .NET Core 3
+
+## Steps to run
+
+```
+git clone https://github.com/SyncfusionSamples/ej2-core-scheduler-with-vue core-with-vue
+cd core-with-vue
+```
+Compile and run the project.
---
+++ VueDemoWithAsp.NetCore/VueDemoWithAsp.NetCore/client-app/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div class="col-md-12 control-section">
+            <div class="content-wrapper">
+                <ejs-schedule id="Schedule"
+                              height="650px">
+                    <e-views>
+                        <e-view option="TimelineDay"></e-view>
+                        <e-view option="TimelineWeek"></e-view>
+                        <e-view option="TimelineWorkWeek"></e-view>
+                        <e-view option="TimelineMonth"></e-view>
+                        <e-view option="Agenda"></e-view>
+                    </e-views>
+                </ejs-schedule>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+    @import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+    @import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+    @import "../node_modules/@syncfusion/ej2-calendars/styles/material.css";
+    @import "../node_modules/@syncfusion/ej2-dropdowns/styles/material.css";
+    @import "../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+    @import "../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+    @import "../node_modules/@syncfusion/ej2-popups/styles/material.css";
+    @import "../node_modules/@syncfusion/ej2-schedule/styles/material.css";
+</style>
+
+<script>
+    import Vue from "vue";
+    import {
+        SchedulePlugin,
+        Agenda,
+        TimelineViews,
+        TimelineMonth,
+        Resize,
+        DragAndDrop
+    } from "@syncfusion/ej2-vue-schedule";
+    Vue.use(SchedulePlugin);
+    export default Vue.extend({
+        provide: {
+            schedule: [Agenda, TimelineViews, TimelineMonth, Resize, DragAndDrop]
+        },
+        methods: {}
+    });
+</script>
+
